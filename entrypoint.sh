9a1978c1be9bae6033e1f4af70899cb169018285
ItsLucas <itslucas@itslucas.me>
2021-08-10 12:08:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 sudo pacman -Syu --noconfirm
 sudo makepkg -si --skippgpcheck
 for pkg in $INPUT_PACKAGES; do
-    yay -S "$pkg" --noconfirm --needed --nopgpfetch --useask --gpgflags "--keyserver pgp.mit.edu" || exit $?
+    yay -S "$pkg" --noconfirm --needed --nopgpfetch --useask --gpgflags "--keyserver keyserver.ubuntu.com" || exit $?
 done
 
 eval $INPUT_SCRIPTS || exit $?
