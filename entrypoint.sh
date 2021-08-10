0b1bd72c3e73c63c82fd67b92bdad1ca20da6a4a
ItsLucas <itslucas@itslucas.me>
2021-08-10 11:58:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 sudo pacman -Syu --noconfirm
 
 for pkg in $INPUT_PACKAGES; do
-    yay -S "$pkg" --noconfirm --needed --useask --gpgflags "--keyserver hkp://pool.sks-keyservers.net" || exit $?
+    yay -S "$pkg" --noconfirm --needed --useask --gpgflags "--keyserver pgp.mit.edu" || exit $?
 done
 
 eval $INPUT_SCRIPTS || exit $?
